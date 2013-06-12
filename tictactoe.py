cbd50ead7c8c95182a13f8e510fa8017d5e4fe9e
Saravanan Vaithianathan <svaithia@uwaterloo.ca>
2013-06-12 00:06:16 -0400

    Added classes for drawing the board etc. Can switch between the two screens using b and p. Can quit by closing the X or pressing ESCAPE

--- tictactoe.py
+++ tictactoe.py
@@ -9,36 +9,49 @@ clock = pygame.time.Clock()
 w = 450
 h = 680
 
-main_screen = pygame.display.set_mode((w,h))
-i_bg = pygame.image.load('images/bg.png')
-i_title = pygame.image.load('images/ttt_main.png')
-i_play = pygame.image.load('images/play.png')
-i_help = pygame.image.load('images/help.png')
-i_about = pygame.image.load('images/about.png')
+class Graphics:
+    i_bg = pygame.image.load('images/bg.png')
+    def buildMainScreen(self):
+        main_screen = pygame.display.set_mode((w,h))
+        i_title = pygame.image.load('images/ttt_main.png')
+        i_play = pygame.image.load('images/play.png')
+        i_help = pygame.image.load('images/help.png')
+        i_about = pygame.image.load('images/about.png')
 
-main_screen.blit(i_bg, (0,0))
-main_screen.blit(i_title, (35,60))
-main_screen.blit(i_play, (42,320))
-main_screen.blit(i_help, (42, 400))
-main_screen.blit(i_about, (112, 490))
+        main_screen.blit(self.i_bg, (0,0))
+        main_screen.blit(i_title, (35,60))
+        main_screen.blit(i_play, (42,320))
+        main_screen.blit(i_help, (42, 400))
+        main_screen.blit(i_about, (112, 490))
 
-i_gtitle = pygame.image.load('images/ttt_title.png')
-i_board = pygame.image.load('images/board.png')
-i_score = pygame.image.load('images/scoreboard.png')
-i_nav = pygame.image.load('images/nav.png')
+    def buildGameScreen(self):
+        i_gtitle = pygame.image.load('images/ttt_title.png')
+        i_board = pygame.image.load('images/board.png')
+        i_score = pygame.image.load('images/scoreboard.png')
+        i_nav = pygame.image.load('images/nav.png')
 
-game_screen = pygame.display.set_mode((w,h))
-game_screen.blit(i_bg, (0,0))
-game_screen.blit(i_gtitle, (30,60))
-game_screen.blit(i_board, (20, 120))
-game_screen.blit(i_score, (55, 540))
-game_screen.blit(i_nav, (35, 610))
+        game_screen = pygame.display.set_mode((w,h))
+        game_screen.blit(self.i_bg, (0,0))
+        game_screen.blit(i_gtitle, (30,60))
+        game_screen.blit(i_board, (20, 120))
+        game_screen.blit(i_score, (55, 540))
+        game_screen.blit(i_nav, (35, 610))
+    
+    screen = 0
 
+graph = Graphics()
 while True:
-   clock.tick(60)
+   clock.tick(60) 
+   if graph.screen==1:
+        graph.buildGameScreen()
+   elif graph.screen==0:
+        graph.buildMainScreen() 
    pygame.display.update()
-   for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-   
 
+   for event in pygame.event.get():
+        if (event.type == pygame.QUIT) or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            sys.exit() 
+        elif event.type == KEYDOWN and event.key == K_p:
+            graph.screen = 1
+        elif event.type == KEYDOWN and event.key == K_b:
+            graph.screen = 0
